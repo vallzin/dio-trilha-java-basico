39320bea2e509694477a519386e080a74da3d84a
vallzin <valmilson@gmail.com>
2024-04-28 09:31:45 -0300

    operador ternário

---
+++ tipos-variaveis/src/OperadorTernario.java
@@ -0,0 +1,20 @@
+public class OperadorTernario {
+    
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 4;
+        b = 3;
+        /*String resultado = "";
+
+        if (a == b) {
+            resultado = "verdadeiro";
+        }else{
+            resultado = "falso";
+        }*/
+
+        String resultado = a==b?"verdadeiro":"falso";
+
+        System.out.println(resultado);
+    }
+}
