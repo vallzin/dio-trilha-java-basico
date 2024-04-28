a7a3d8dcb2071055c3d4d80a2309e008c9ea99b0
vallzin <valmilson@gmail.com>
2024-04-28 08:13:49 -0300

    concatenação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,6 +3,20 @@ public class Operadores {
     public static void main(String[] args) {
         String  nomeCompleto = "Linguagem " + "JAVA";
 
-        System.out.println(nomeCompleto);
+        System.out.println("\n"+nomeCompleto);
+        
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+        concatenacao = 1 + 1 +  1 + "1";
+        System.out.println(concatenacao);
+        concatenacao = 1 + "1" +  1 + 1;
+        System.out.println(concatenacao);
+        concatenacao = 1 + "1" +  "1" + "1";
+        System.out.println(concatenacao);
+        concatenacao = "1" + 1 + 1 + 1;
+        System.out.println(concatenacao);
+        concatenacao = "1" + (1 + 1 + 1);
+        System.out.println(concatenacao);
+
     }
 }
