ff0f271f6a88c5cc23cbf0d4b6d23708f22ec8cc
vallzin <valmilson@gmail.com>
2024-04-28 10:57:51 -0300

    operadores relacionais

---
+++ tipos-variaveis/src/OperadoresRelacionais.java
@@ -0,0 +1,16 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        int numeroUm = 1;
+        int numeroDois = 2;
+
+        boolean simNao = numeroUm == numeroDois;
+
+        System.out.println("O numeroUm é igual a numeroDois? "+simNao);
+
+        simNao = numeroUm != numeroDois;
+
+        System.out.println("O numeroUm é diferente de numeroDois? "+simNao);
+
+        
+    }
+}
