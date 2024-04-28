315cdee5cfdc1e4a1a16e0e1cd1313972df24c09
vallzin <valmilson@gmail.com>
2024-04-28 08:36:35 -0300

    operador unários

---
+++ tipos-variaveis/src/OperadoresUnario.java
@@ -0,0 +1,19 @@
+public class OperadoresUnario {
+    
+    public static void main(String[] args) {
+        int numero = 4;
+        System.out.println(numero);
+        System.out.println(- numero);
+
+        numero = - numero;
+        System.out.println(numero);
+        
+        numero = numero * -1;
+        System.out.println(numero);
+        
+        numero ++;
+        System.out.println(numero);
+        System.out.println(numero++);
+        System.out.println(numero);
+    }
+}
