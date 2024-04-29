6e5c2e0f74c5d47058b1fc93001c6f691382d219
vallzin <valmilson@gmail.com>
2024-04-29 15:31:13 -0300

    esecução pelo terminal

--- java-terminal/src/MinhaClasse.java
+++ java-terminal/src/MinhaClasse.java
@@ -1,5 +1,6 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.println("Oi, fui executado pelo Terminal");
+        System.out.println("Oi, Turma, fui executado pelo Terminal");
     }
+    
 }
