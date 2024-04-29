25fe82b014a5b64a93410a3e82b49c428f795c54
vallzin <valmilson@gmail.com>
2024-04-29 16:41:48 -0300

    atualizando arquivo

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,7 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
     public static void main(String[] args) {
         //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in)
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
 
         System.out.println("Ola, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
